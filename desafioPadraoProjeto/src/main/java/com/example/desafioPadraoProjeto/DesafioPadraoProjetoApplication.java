5290bfcae676a24b34f7e855c22a86cedf4a80fc
André Luiz de Oliveira <andreloliveira28@gmail.com>
2025-08-17 13:53:14 -0300

    Criação do projeto

---
+++ desafioPadraoProjeto/src/main/java/com/example/desafioPadraoProjeto/DesafioPadraoProjetoApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafioPadraoProjeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPadraoProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPadraoProjetoApplication.class, args);
+	}
+
+}
---
+++ desafioPadraoProjeto/src/test/java/com/example/desafioPadraoProjeto/DesafioPadraoProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafioPadraoProjeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPadraoProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
